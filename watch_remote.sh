12ce5928d6f46996c8be91fe44060d2011cd3998
Johannes Filter <hi@jfilter.de>
2020-02-27 18:06:38 +0100

    remove dev script

--- watch_remote.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-set -x
-
-# needs https://github.com/kimmobrunfeldt/chokidar-cli
-
-rsync -vrazh --exclude=".*" . sn:/home/filter/code/hyperhyper/ &&
-chokidar "**/*.*" -c "rsync -vrazh --exclude=".*" . sn:/home/filter/code/hyperhyper/"
