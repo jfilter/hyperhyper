107e4e2002cb12ac4a96ee0173e444a67e599b01
Johannes Filter <hi@jfilter.de>
2020-10-24 01:00:10 +0200

    add missing __version__

--- hyperhyper/__init__.py
+++ hyperhyper/__init__.py
@@ -8,3 +8,5 @@ from .corpus import Corpus, Vocab
 from .preprocessing import texts_to_sents, tokenize_texts, tokenize_texts_parallel
 
 logging.getLogger(__name__).addHandler(NullHandler())
+
+__version__ = "0.1.1"
